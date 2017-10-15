731478e8cd644e5dd5f8d757652252b906e0bdb7
PoSeiDonTeaM <johnkotsas@me.com>
2017-10-15 22:08:26 +0300

    giannis code

--- nick_exercise.c
+++ nick_exercise.c
@@ -1,23 +1,17 @@
 #include <stdio.h>
 
-
 int main()
-
-
 {
-int E1, E2, M1,M2,H1,H2;                            // Definition of variables
-scanf("%d %d %d %d %d %d",E1,E2,M1,M2,H1,H2);       // User's inputs
-int AE= abs(E1-E2);                                 // Year's difference
-int AM =abs(M1-M2);                                 // Month's difference
-
-if (AM == 0)                                        // If the 2 Dates are on the same month make absolute substraction        
+int E1, E2, M1,M2,H1,H2;
+scanf("%d %d %d %d %d %d",E1,E2,M1,M2,H1,H2);
+int AE= abs(E1-E2);
+int AM =abs(M1-M2);
+if AM=0 
 {
   AH=abs(H1-H2);
-}  
-else                                                // If the 2 Dates are not on the same month check which date is earlier and take the remainder 
-{                                                   // of the earlier day and the date of the second one and sum them.
+else
  int a=30-H1;
- int AH= a +(AM*30)+ H2;
+ int AH=a +(AM*30)+ H2;
  int AM= AH/30;
  int AH=AH%30;
  }
